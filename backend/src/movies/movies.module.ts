97fcd6cbab518b6f5ae571f9bc7526d1ba0586af
GARNIER-Emmanuel <manu.boidun@gmail.com>
2026-05-12 14:43:17 +0200

    feat(movies): enregistrer les composants trailer dans MoviesModule (GREEN)

--- backend/src/movies/movies.module.ts
+++ backend/src/movies/movies.module.ts
@@ -3,9 +3,19 @@ import { MoviesController } from './movies.controller';
 import { MoviesService } from './movies.service';
 import { GetCineScrollMoviesController } from './get-cinescroll-movies/get-cinescroll-movies.controller';
 import { GetCineScrollMoviesService } from './get-cinescroll-movies/get-cinescroll-movies.service';
+import { GetMovieTrailerController } from './get-movie-trailer/get-movie-trailer.controller';
+import { GetMovieTrailerService } from './get-movie-trailer/get-movie-trailer.service';
 
 @Module({
-  controllers: [MoviesController, GetCineScrollMoviesController],
-  providers: [MoviesService, GetCineScrollMoviesService],
+  controllers: [
+    MoviesController, 
+    GetCineScrollMoviesController,
+    GetMovieTrailerController
+  ],
+  providers: [
+    MoviesService, 
+    GetCineScrollMoviesService,
+    GetMovieTrailerService
+  ],
 })
 export class MoviesModule {}
